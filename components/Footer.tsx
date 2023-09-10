8f78870b23e00e930b28ee0cca612892f4a598b3
westhouseK <nishiiekazuki@gmail.com>
2023-09-10 17:01:32 +0900

    [fix] Header,Footer,Contentsの分離

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100">
+      <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center">
+        <div>© 備忘録</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
