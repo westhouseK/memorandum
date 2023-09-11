e0af87920650f155859ef9ad9a4af55d2bfd4ec7
westhouseK <nishiiekazuki@gmail.com>
2023-09-12 00:00:47 +0900

    [add] 一覧画面に画像を表示する対応

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { readFileSync, readdirSync } from "fs"
 import path from "path"
 
+import Image from "next/image"
 import Link from "next/link"
 
 import matter from "gray-matter"
@@ -14,6 +15,9 @@ async function getArticles() {
       slug,
       data,
     }
+  }).sort((a, b) => {
+    // 投稿日時の昇順
+    return new Date(a.data.createdAt) < new Date(b.data.createdAt) ? 1 : -1
   })
 }
 
@@ -21,11 +25,24 @@ export default async function Home() {
   const articles = await getArticles();
   return (
     <div className="my-8">
-      {articles.map(v => (
-        <div key={v.slug}>
-          <Link href={`/article/${v.slug}`}>{v.data.title}</Link>
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {articles.map(v => (
+          <Link href={`/article/${v.slug}`} key={v.slug}>
+            <div className="border rounded-lg">
+              <Image
+                src={`/image/${v.slug}/main_visual.jpg`}
+                width={1200}
+                height={700}
+                alt={v.data.title}
+              />
+            </div>
+            <div className="px-2 py-4">
+              <h1 className="font-bold text-lg">{v.data.title}</h1>
+              <span>{v.data.createdAt}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 border-b z-10 bg-white">
       <div className="max-w-4xl mx-auto flex justify-between items-center h-12">
-        <Link href="/">LOGO</Link>
+        <Link href="/">memorandum</Link>
         <div>Link</div>
       </div>
     </header>
