eb10c74650925c7e918bd67dd4d2c31c6190ed2c
westhouseK <nishiiekazuki@gmail.com>
2023-09-11 20:39:17 +0900

    [fix] マークダウンをHTMLにパースする対応

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -2,15 +2,26 @@ import { readFileSync } from "fs"
 import path from "path"
 
 import matter from "gray-matter"
+import rehypeStringify from "rehype-stringify"
+import remarkParse from "remark-parse"
+import remarkRehype from "remark-rehype"
+import { unified } from "unified"
 
 async function getArticle(slug: string) {
-  return matter(readFileSync(path.join(process.cwd(), "articles", slug, "index.md")), "utf-8")
+  const { data, content } = matter(readFileSync(path.join(process.cwd(), "articles", slug, "index.md")))
+  return {
+    data,
+    content: (await unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeStringify)
+      .process(content)).toString()
+  }
 }
 
 export default async function Article({ params }: { params: { slug: string } }) {
-  const article = await getArticle(params.slug)
-  console.log(article)
+  const { data, content } = await getArticle(params.slug)
   return (
-    <div>a</div>
+    <div dangerouslySetInnerHTML={{ __html: content }}></div>
   )
 }
